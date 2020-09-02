6d1c74f3785aed0d734017bab1f1f64d7c4bc0cf
Saltysam101 <55891755+Saltysam101@users.noreply.github.com>
2020-09-02 13:19:28 -0400

    added link to my github page

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../home.css';
 import image from '../Assets/damian-zaleski-RYyr-k3Ysqg-unsplash(1).png';
 import icon from '../Assets/iconfinder_github-social-media_765246(1).png';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
     render() {
@@ -10,8 +11,10 @@ export default class Home extends Component {
                 <div className="hero-page">
                     <img className="hero-bg-img" src={image} alt=""/>
                     <h1 className="hero-stmt">I can help expand your interests with a website!</h1>
-                    <h3>My Github:</h3>
-                    <img src={icon} alt=""/>
+                    <h3 className="hero-git-title">My Github:</h3>
+                    <Link className="hero-git-link" target="_blank" to={"//github.com/Saltysam101"}>
+                        <img className="hero-git-icon" src={icon} alt=""/>
+                    </Link>
                 </div>
             </div>
         )
