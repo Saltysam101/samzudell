d1bc4bb9114b3bcdbf44537b2251468b9c00b20d
Saltysam101 <s.zudell98@gmail.com>
2020-10-08 13:40:04 -0400

    added firebase and some logic for sending emails

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../styles/contact.css';
 
 export default class Contact extends Component {
     render() {
         return (
             <div>
-                Contact
+                <form action="">
+
+                    <label htmlFor="name">Name</label>
+                    <input placeholder="Name" name="name" type="text"/>
+
+                    <label htmlFor="email">Email</label>
+                    <input placeholder="Email" type="email" name="email" id="email"/>
+
+                    <label htmlFor="message">Message</label>
+                    <textarea placeholder="Message" name="message" id="message" cols="30" rows="10"></textarea>
+
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
