ca25373fae15e3166aa0037fb472077922237f8e
Saltysam101 <s.zudell98@gmail.com>
2020-10-09 23:28:16 -0400

    commit

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,20 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import '../styles/contact.css';
+import db from '../components/firebase';
+
+
 
 export default class Contact extends Component {
+
+
+const [name, setName] = useState("");
+const [email, setEmail] = useState("");
+const [message, setMessage] = useState("");
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    db.collection('contacts').add({
+        name: name,
+        email: email,
+        message: message
+    })
+    .then(() => {
+        alert('Message has been submitted')
+    })
+    .catch((error) => {
+        alert(error.message)
+    })
+
+    setName("");
+    setEmail("");
+    setMessage("");
+}
+
     render() {
         return (
             <div>
-                <form action="">
+                <form onSubmit={handleSubmit} >
 
                     <label htmlFor="name">Name</label>
-                    <input placeholder="Name" name="name" type="text"/>
+                    <input 
+                    placeholder="Name"
+                    name="name" 
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}/>
 
                     <label htmlFor="email">Email</label>
-                    <input placeholder="Email" type="email" name="email" id="email"/>
+                    <input 
+                    placeholder="Email" 
+                    type="email" 
+                    name="email"
+                    value={email}
+                    id="email"
+                    onChange={(e) => setEmail(e.target.value)} />
 
                     <label htmlFor="message">Message</label>
-                    <textarea placeholder="Message" name="message" id="message" cols="30" rows="10"></textarea>
+                    <textarea 
+                    placeholder="Message" 
+                    name="message" 
+                    id="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    cols="30" 
+                    rows="10"></textarea>
 
                     <button type="submit">Submit</button>
                 </form>
