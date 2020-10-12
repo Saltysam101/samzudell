c80988a023cb0a07c9cb4f393a6aa19961d46a61
Saltysam101 <s.zudell98@gmail.com>
2020-10-12 09:26:01 -0400

    styled contact page for all layouts

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Axios, db } from '../firebase/firebaseconfig'
+import '../styles/contact.css'
 
 
 const ContactForm = () => {
@@ -41,28 +42,41 @@ const ContactForm = () => {
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          onChange={updateInput}
-          value={formData.name || ''}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          onChange={updateInput}
-          value={formData.email || ''}
-        />
-        <textarea
-          type="text"
-          name="message"
-          placeholder="Message"
-          onChange={updateInput}
-          value={formData.message || ''}
-        ></textarea>
-        <button type="submit">Submit</button>
+        <div className="name-container">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={updateInput}
+            value={formData.name || ''}
+            required
+          />
+        </div>
+        <div className="email-container">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={updateInput}
+            value={formData.email || ''}
+            required
+          />
+        </div>
+
+        <div className="message-container">
+          <label htmlFor="message">Message</label>
+          <textarea
+            type="text"
+            name="message"
+            placeholder="Message"
+            onChange={updateInput}
+            value={formData.message || ''}
+            required
+          ></textarea>
+        </div>
+        <button className="form-submit" type="submit">Submit</button>
       </form>
     </>
   )
