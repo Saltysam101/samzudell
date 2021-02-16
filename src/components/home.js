29d8b04b661deef023bdd1f5d3f9fb73cb20d92b
Saltysam101 <s.zudell98@gmail.com>
2021-02-16 10:10:35 -0500

    finished mobile redesign of homepage, just need to change thumbnails nav menu

--- src/components/home.js
+++ src/components/home.js
@@ -11,21 +11,19 @@ export default class Home extends Component {
     
     render() {
         return (
-            <div>
+            <div className="main">
                 <div className="hero-page">
                     <div className="hero-info">
                         <div className="hero-stmt-container">
                             <h1 className="hero-stmt">Samuel Zudell</h1>
                             <p>A problem solver with skills in Web Development.</p>
                         </div>    
-                        <div className="git-container">
                             <Link className="hero-git-link" target="_blank" to={"//github.com/Saltysam101"}>
                                 <button className="git-link-btn">  
                                     <span>Github</span>
                                     <img className="hero-git-icon" src={icon} alt=""/>
                                 </button>
                             </Link>
-                        </div>
                     </div>
                     <div className="svg-container">
                         <svg xmlns="http://www.w3.org/2000/svg" width="1108" height="880" viewBox="0 0 1108 880" fill="none">
@@ -406,41 +404,35 @@ export default class Home extends Component {
                     <div className="project-container">
                         <div className="project-item">
                             <h3 className="project-name">Current Weather</h3>
-                            <Link target="_blank" to={"//www.currentweathertemp.com/"}>
+                            <Link target="_blank" className="img-link" to={"//www.currentweathertemp.com/"}>
                                 <img className="project-img" loading="eager" src={currentWeather} alt=""/>
                             </Link>
-                            <div className="div">
-                                <p className="project-description">
+                            <p className="project-description">
                                     This is a basic weather website. It will ask for permission to track your location and then based on that it will tell you some basic weather info for that location. The site will also give you a 3 day forecast that is updated every 3 hours.
-                                </p>
-                                <Link className="git-link" target="_blank" to={"//github.com/Saltysam101/weather-js"}>
-                                    <button className="see-code">See Code</button>
-                                </Link>
-                            </div>
+                            </p>
+                            <Link className="git-link" target="_blank" to={"//github.com/Saltysam101/weather-js"}>
+                                <button className="see-code">Code</button>
+                            </Link>
                         </div>
                         <div className="project-item">
                             <h3 className="project-name">Petsy</h3>
-                            <Link target="_blank" to={"//www.dogsstuff.net/"}>
+                            <Link target="_blank" className="img-link" to={"//www.dogsstuff.net/"}>
                                 <img className="project-img" loading="lazy" src={petsy} alt=""/>
                             </Link>
-                            <div className="div">
-                                <p className="project-description">Petsy is a make-believe dog store. It sells different brands of dog food and dog toys. When you click the 'add to cart' button, it adds it to the cart and if you want more than just one of that item, you can adjust the quantity in the cart. The total should update to adding items and adjustments to the quantities. Finally, when you click the 'checkout' button, an alert will pop up and let you know your "purchase" went through and it will clear the cart.</p>
-                                <Link className="git-link" target="_blank" to={"//github.com/Saltysam101/petsy"}>
-                                    <button className="see-code">See Code</button>
-                                </Link>
-                            </div>
+                            <p className="project-description">Petsy is a make-believe dog store. It sells different brands of dog food and dog toys. When you click the 'add to cart' button, it adds it to the cart and if you want more than just one of that item, you can adjust the quantity in the cart. The total should update to adding items and adjustments to the quantities. Finally, when you click the 'checkout' button, an alert will pop up and let you know your "purchase" went through and it will clear the cart.</p>
+                            <Link className="git-link" target="_blank" to={"//github.com/Saltysam101/petsy"}>
+                                <button className="see-code">Code</button>
+                            </Link>
                         </div>
                         <div className="project-item">
                             <h3 className="project-name">ChatMe</h3>
-                            <Link target="_blank" to={"//chatmeapp.net/"}>
+                            <Link target="_blank" className="img-link" to={"//chatmeapp.net/"}>
                                 <img className="project-img" loading="lazy" src={chatme} alt=""/>
                             </Link>
-                            <div className="div">
-                                <p className="project-description">ChatMe is a basic realtime chat site. You can create an account and just chat with anyone who is on there. There are no seperate groups for different chats, it's all just one chat.</p>
+                            <p className="project-description">ChatMe is a basic realtime chat site. You can create an account and just chat with anyone who is on there. There are no seperate groups for different chats, it's all just one chat.</p>
                                 <Link className="git-link" target="_blank" to={"//github.com/Saltysam101/ChatMe"}>
-                                    <button className="see-code">See Code</button>
-                                </Link>
-                            </div>
+                                <button className="see-code">Code</button>
+                            </Link>
                         </div>
                     </div>
 
